b68060c0160368857eb13c32bfa588a032508568
Oleh Poliakov <106983325+OlehPoliakov@users.noreply.github.com>
2022-08-22 18:01:34 +0300

    feature/1-task

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,24 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.startBtn.addEventListener('click', btnClick)
+
+function btnClick() {
+    const timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+        refs.startBtn.disabled = true;
+        refs.stopBtn.disabled = false;
+    }, 1000);
+
+    refs.stopBtn.addEventListener("click", () => {
+        clearInterval(timerId);
+        refs.startBtn.disabled = false;
+        refs.stopBtn.disabled = true;
+    });
+}
